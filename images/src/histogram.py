de1d3c46d80e88cd967a0e0dc6d5856698ae55ef
B00144121 <146427610+B00144121@users.noreply.github.com>
2026-03-03 15:33:15 +0000

    analysis file made

---
+++ images/src/histogram.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def compute_historgram(gray):
+    hist = np.zeros(256, dtype=np.int64)
+    pixels = gray.flaten()
+
+
+    for v in pixels:
+        hist[int(v)] += 1
+
+        
+
+    return hist
