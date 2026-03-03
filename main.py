ebc38615e0a3e244838c0448e7d5f467df88b556
B00144121 <146427610+B00144121@users.noreply.github.com>
2026-03-03 16:49:34 +0000

    modified to allow for image to be processed as grayscale nad binary also timer for the threshold

---
+++ main.py
@@ -0,0 +1,52 @@
+import cv2 as cv
+import numpy as np
+import time
+
+from src.thresholding import otsu_threshold, apply_threshold
+
+
+image_paths = [
+    "images/Oring1.jpg",
+    "images/Oring2.jpg"
+]
+
+#loops through each image path
+for path in image_paths:
+
+    #reads images into memory
+    img = cv.imread(path)
+
+    if img is None:
+        print("Error: Could not read image at path: " + path)
+        continue
+
+    #convert to grayscale
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    start = time.perf_counter()
+
+    
+
+    t = otsu_threshold(gray)
+
+    binary = apply_threshold(gray, t)
+
+    end = time.perf_counter()
+
+    print(path, "threshold:", t)
+    print(path, "threshold:", t, "|time:", f"{(end-start): .4f} seconds")
+
+    cv.imshow("Original", img)
+    cv.imshow("Grayscale", gray)
+    cv.imshow("Binary", binary)
+    cv.waitKey(0)
+
+
+    
+
+    
+
+#Put classification result on screen
+
+cv.waitKey(0)
+cv.destroyAllWindows()
